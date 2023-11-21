8f45ff5ad1fe9e917eed47df25c1a676bd892d38
Guilherme Schmidt <129059738+GuiRDx@users.noreply.github.com>
2023-11-21 20:58:54 -0300

    Prova Regimental 3Trimestre

---
+++ ATIVIDADES JS,HTML,CSS/ProvaMenon3TriFront/index.js
@@ -0,0 +1,161 @@
+function cbr1000rr() {
+    document.getElementById("corte_cbr1000rr").play();
+}
+
+document.querySelector(".tecla_cbr1000rr").onclick = cbr1000rr;
+//essa função abaixo é pra para o som//
+var audioCbr1000rr = document.getElementById("corte_cbr1000rr");
+function toggleCbr1000rr() {
+    if (audioCbr1000rr.paused) {
+        audioCbr1000rr.play();
+    } else {
+        audioCbr1000rr.pause();
+        audioCbr1000rr.currentTime = 0;
+    }
+}
+document.querySelector(".tecla_cbr1000rr").onclick = toggleCbr1000rr;
+
+
+
+//proxima//
+function h2r() {
+    document.getElementById("corte_h2r").play();
+}
+
+document.querySelector(".tecla_h2r").onclick = h2r;
+var audioH2r = document.getElementById("corte_h2r");
+
+function toggleH2r() {
+    if (audioH2r.paused) {
+        audioH2r.play();
+    } else {
+        audioH2r.pause();
+        audioH2r.currentTime = 0;
+    }
+}
+document.querySelector(".tecla_h2r").onclick = toggleH2r;
+
+//proxima//
+
+
+
+function hornet() {
+    document.getElementById("corte_hornet").play();
+}
+
+document.querySelector(".tecla_hornet").onclick = hornet;
+var audioHornet = document.getElementById("corte_hornet");
+function toggleHornet() {
+    if (audioHornet.paused) {
+        audioHornet.play();
+    } else {
+        audioHornet.pause();
+        audioHornet.currentTime = 0;
+    }
+}
+document.querySelector(".tecla_hornet").onclick = toggleHornet;
+
+// Próxima //
+
+function miliduque() {
+    document.getElementById("corte_miliduque").play();
+}
+
+document.querySelector(".tecla_miliduque").onclick = miliduque;
+var audioMiliduque = document.getElementById("corte_miliduque");
+function toggleMiliduque() {
+    if (audioMiliduque.paused) {
+        audioMiliduque.play();
+    } else {
+        audioMiliduque.pause();
+        audioMiliduque.currentTime = 0;
+    }
+}
+document.querySelector(".tecla_miliduque").onclick = toggleMiliduque;
+//proxima//
+
+function mt09(){
+    document.getElementById("corte_mt09").play();
+}
+document.querySelector(".tecla_mt09").onclick = mt09;
+var audioMt09 = document.getElementById("corte_mt09");
+
+function toggleMt09() {
+    if (audioMt09.paused) {
+        audioMt09.play();
+    } else {
+        audioMt09.pause();
+        audioMt09.currentTime = 0;
+    }
+}
+document.querySelector(".tecla_mt09").onclick = toggleMt09;
+
+//proxima//
+
+function Tiger() {
+    document.getElementById("corte_tiger").play();
+}
+document.querySelector(".tecla_tiger").onclick = Tiger;
+var audioTiger = document.getElementById("corte_tiger");
+
+function toggleTiger() {
+    if (audioTiger.paused){
+        audioTiger.play();
+    } else {
+        audioTiger.pause();
+        audioTiger.currentTime = 0;
+    }
+}
+document.querySelector(".tecla_tiger").onclick = toggleTiger;
+
+//proxima//
+
+
+function xj6(){
+    document.getElementById("corte_xj6").play();
+}
+document.querySelector(".tecla_xj6").onclick = xj6;
+var audioXj6 = document.getElementById("corte_xj6");
+function toggleXj6() {
+    if (audioXj6.paused) {
+        audioXj6.play();
+    } else {
+        audioXj6.pause();
+        audioXj6.currentTime = 0;
+    }
+}
+document.querySelector(".tecla_xj6").onclick = toggleXj6;
+
+//proxima//
+
+function Xt660(){
+    document.getElementById("corte_xt660").play();
+}
+document.querySelector(".tecla_xt660").onclick = Xt660;
+var audioXt660 = document.getElementById("corte_xt660");
+function toggleXt660() {
+    if (audioXt660.paused) {
+        audioXt660.play();
+    } else {
+        audioXt660.pause();
+        audioXt660.currentTime = 0;
+    }
+}
+document.querySelector(".tecla_xt660").onclick = toggleXt660;
+
+//proxima//
+
+function Z1000(){
+    document.getElementById("corte_z1000").play();
+}
+document.querySelector(".tecla_z1000").onclick = Z1000;
+var audioZ1000 = document.getElementById("corte_z1000");
+function toggleZ1000() {
+    if (audioZ1000.paused) {
+        audioZ1000.play();
+    } else {
+        audioZ1000.pause();
+        audioZ1000.currentTime = 0;
+    }
+}
+document.querySelector(".tecla_z1000").onclick = toggleZ1000;
